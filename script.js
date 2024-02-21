d38459cdb3bfe376442134051386227cdf96e405
Faraz Ahmad <102180103+FarazAhmad2@users.noreply.github.com>
2024-02-21 15:46:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+function daysOfAYear(year) {
+    year = parseInt(year);
+    if ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0) {
+        return 366; // Leap year
+    }
+    return 365; // Non-leap year
+}
+
+const year  = prompt("Enter a year:");
+alert(daysOfAYear(year));
